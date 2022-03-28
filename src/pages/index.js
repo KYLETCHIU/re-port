9583f238699835ce819d75adfc7d06582c855090
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2022-03-28 19:30:03 -0400

    Homepage
    Worked on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import CP from '../cp.png';
+
 const Home = () => {
   return (
-    <div className="bdy">
-      <div className="title">
-        <h1>Welcome to my Portfolio</h1>
-      </div>
-      <div className="cardd">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img roundedCirclevariant="top" src={CP} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+    <>
+      <div className="bdy">
+        <div className="title">
+          <h1>Welcome to my Portfolio</h1>
+          <h1>Pull up a chair</h1>
+          <h1>Stay awhile</h1>
+        </div>
+          
       </div>
 
-
-    </div>
+    </>
   );
 };
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,102 +1,111 @@
 import React from 'react';
-import { Card, CardGroup, Button} from 'react-bootstrap';
+import { Card, CardGroup, Button } from 'react-bootstrap';
+import gH from '../gH.png';
+import WDS from '../WDS.png';
+import CQ from '../CQ.png';
+import TE from '../TE.png';
+import WD from '../WD.png';
+import OP from '../OP.png';
+
 const Portfolio = () => {
   return (
     <>
-    <div className="title">
-      <h1>My apps</h1>
-    </div>
+      <div className="porty">
+        <div className="title">
+          <h1>My apps</h1>
+        </div>
 
-<CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        Test Text
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Be sure to check out the GitHub Repo</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-      Test Text
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Be sure to check out the GitHub Repo</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-      Test Text
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Be sure to check out the GitHub Repo</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
-<CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-      Test Text
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Be sure to check out the GitHub Repo</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-      Test Text
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Be sure to check out the GitHub Repo</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-      Test Text
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Be sure to check out the GitHub Repo</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
-</>
+        <CardGroup className="crdgrp">
+          <Card className="portcards">
+            <Card.Img variant="top" src={gH} />
+            <Card.Body className="crdbody">
+              <Card.Title>gitHomies</Card.Title>
+              <Card.Text>
+                A way for developers to collaberate and stay in touch.
+              </Card.Text>
+              <Button variant="primary">App</Button>
+              <Button variant="primary">GitHub Repo</Button>
+            </Card.Body>
+            <Card.Footer className ="crdfooter">
+              <small className="text-muted">Be sure to check out the GitHub Repo</small>
+            </Card.Footer>
+          </Card>
+          <Card className="portcards">
+            <Card.Img variant="top" src={WDS} />
+            <Card.Body className="crdbody">
+              <Card.Title>Word Day Scheduler</Card.Title>
+              <Card.Text>
+                A simple way to play out your day.
+              </Card.Text>
+              <Button variant="primary">App</Button>
+              <Button variant="primary">GitHub Repo</Button>
+            </Card.Body>
+            <Card.Footer className ="crdfooter">
+              <small className="text-muted">Be sure to check out the GitHub Repo</small>
+            </Card.Footer>
+          </Card>
+          <Card className="portcards">
+            <Card.Img variant="top" src={CQ} />
+            <Card.Body className="crdbody">
+              <Card.Title>Code Quiz</Card.Title>
+              <Card.Text>
+                Test your knowledge of coding with a fun quiz.
+              </Card.Text>
+              <Button variant="primary">App</Button>
+              <Button variant="primary">GitHub Repo</Button>
+            </Card.Body>
+            <Card.Footer className ="crdfooter">
+              <small className="text-muted">Be sure to check out the GitHub Repo</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+        <CardGroup className="crdgrp">
+          <Card className="portcards">
+            <Card.Img variant="top" src={TE} />
+            <Card.Body className="crdbody">
+              <Card.Title>J.A.T.E</Card.Title>
+              <Card.Text>
+                Just Another Text Editor
+              </Card.Text>
+              <Button variant="primary">App</Button>
+              <Button variant="primary">GitHub Repo</Button>
+            </Card.Body>
+            <Card.Footer className ="crdfooter">
+              <small className="text-muted">Be sure to check out the GitHub Repo</small>
+            </Card.Footer>
+          </Card>
+          <Card className="portcards">
+            <Card.Img variant="top" src={WD} />
+            <Card.Body className="crdbody">
+              <Card.Title>Weather Dashboard</Card.Title>
+              <Card.Text>
+                Is it going to rain? Find out here.
+              </Card.Text>
+              <Button variant="primary">App</Button>
+              <Button variant="primary">GitHub Repo</Button>
+            </Card.Body>
+            <Card.Footer className ="crdfooter">
+              <small className="text-muted">Be sure to check out the GitHub Repo</small>
+            </Card.Footer>
+          </Card>
+          <Card className="portcards">
+            <Card.Img variant="top" src={OP} />
+            <Card.Body className="crdbody">
+              <Card.Title>Dev Portfolio</Card.Title>
+              <Card.Text>
+                Same as what youre on now..but the retro version
+              </Card.Text>
+              <Button variant="primary">App</Button>
+              <Button variant="primary">GitHub Repo</Button>
+            </Card.Body>
+            <Card.Footer className ="crdfooter">
+              <small className="text-muted">Be sure to check out the GitHub Repo</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+      </div>
+    </>
   );
 };
-  
+
 export default Portfolio;
